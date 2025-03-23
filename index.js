84573e2479e7feeca5d7f7a60d226a0fd25ce60d
Neema <neema.edward@student.moringaschool.com>
2025-03-23 20:55:33 +0300

    adds DOM elements

---
+++ index.js
@@ -0,0 +1,9 @@
+const baseURL = "http://localhost:3000/characters";
+
+const characterBar = document.getElementById("character-bar");
+const detailedInfo = document.getElementById("detailed-info");
+const nameDisplay = document.getElementById("name");
+const imageDisplay = document.getElementById("image");
+const voteCount = document.getElementById("vote-count");
+const votesForm = document.getElementById("votes-form");
+
